2dbbbbbcfe011ecdde39726427eea0bf41f584ef
Boris Drybin <makoh93@gmail.com>
2023-05-24 19:01:57 +0300

    second task added

--- Program.cs
+++ Program.cs
@@ -10,6 +10,76 @@
 // 8 4 4 2
 
 
+// Console.WriteLine("Введите количество строк массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
+
+// Console.WriteLine("Введите количество столбцов массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
+
+// int[,] arr = new int [rows, columns];
+
+// void GetArray()
+// {
+//     for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArray()
+// {
+//    for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void ChangeArray()
+// {
+
+//     for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             for(int k = 0; k < arr.GetLength(1) - 1; k++)
+//             {
+//                 if(arr[i, k] < arr[i, k + 1])
+//                 {
+//                     int temp = arr[i, k + 1];
+//                     arr[i, k + 1] = arr[i, k];
+//                     arr[i, k] = temp;
+//                 }
+//             }
+//         }
+        
+//     }
+// }
+
+// GetArray();
+// PrintArray();
+// ChangeArray();
+// PrintArray();
+
+
+// Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и 
+// выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+
 Console.WriteLine("Введите количество строк массива");
 int rows = Convert.ToInt32(Console.ReadLine());
 
@@ -42,28 +112,32 @@ void PrintArray()
     Console.WriteLine();
 }
 
-void ChangeArray()
+void SumRow(int[,] array)
 {
+    int minSum = 0;
+    int index = 0;
 
     for(int i = 0; i < arr.GetLength(0); i++)
-    {
-        for(int j = 0; j < arr.GetLength(1); j++)
         {
-            for(int k = 0; k < arr.GetLength(1) - 1; k++)
-            {
-                if(arr[i, k] < arr[i, k + 1])
+            int sum = 0;
+
+            for(int j = 0; j < arr.GetLength(1); j++)
                 {
-                    int temp = arr[i, k + 1];
-                    arr[i, k + 1] = arr[i, k];
-                    arr[i, k] = temp;
+                    sum = sum + arr[i, j];
                 }
-            }
+            if(i == 0)
+                {
+                    minSum = sum;
+                }
+            else if(sum < minSum)
+                {
+                    minSum = sum;
+                    index = i;
+                }
         }
-        
-    }
+        Console.WriteLine("Номер строки с минимальной суммой: " +(index + 1));
 }
 
 GetArray();
 PrintArray();
-ChangeArray();
-PrintArray();
+SumRow(arr);
