5d068512bfdf99f0f22acfaa8ad2a71ee70a4935
Boris Drybin <makoh93@gmail.com>
2023-05-24 21:41:04 +0300

    fifth task added

--- Program.cs
+++ Program.cs
@@ -234,74 +234,137 @@
 // 27(0,0,1) 90(0,1,1)
 // 26(1,0,1) 55(1,1,1)
 
-Console.WriteLine("Введите x");
-int x = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите x");
+// int x = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите y");
-int y = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите y");
+// int y = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите z");
-int z = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите z");
+// int z = Convert.ToInt32(Console.ReadLine());
 
-int[,,] arr3D = new int [x, y, z];
+// int[,,] arr3D = new int [x, y, z];
 
-void FillArray(int[,,] arr3D)
+// void FillArray(int[,,] arr3D)
+// {
+//     int[] temp = new int[arr3D.GetLength(0) * arr3D.GetLength(1) * arr3D.GetLength(2)];
+    
+//     int  number;
+    
+//     for (int i = 0; i < temp.GetLength(0); i++)
+//     {
+//         temp[i] = new Random().Next(10, 100);
+        
+//         number = temp[i];
+        
+//         if (i >= 1)
+//         {
+//             for (int j = 0; j < i; j++)
+//             {
+//                 while (temp[i] == temp[j])
+//                 {
+//                     temp[i] = new Random().Next(10, 100);
+//                     j = 0;
+//                     number = temp[i];
+//                 }
+//                 number = temp[i];
+//             }
+//         }
+//     }
+    
+//     int count = 0; 
+    
+//     for (int x = 0; x < arr3D.GetLength(0); x++)
+//     {
+//         for (int y = 0; y < arr3D.GetLength(1); y++)
+//         {
+//             for (int z = 0; z < arr3D.GetLength(2); z++)
+//             {
+//                 arr3D[x, y, z] = temp[count];
+//                 count++;
+//             }
+//         }   
+//     }
+// }
+
+// void WriteArray (int[,,] arr3D)
+// {
+//     for (int i = 0; i < arr3D.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < arr3D.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < arr3D.GetLength(2); k++)
+//             {
+//                 Console.Write( $"{arr3D[i,j,k]}({k},{i},{j})  ");
+//             }
+//             Console.WriteLine();
+//         }
+//         Console.WriteLine();
+//     }
+// }
+
+// FillArray(arr3D);
+// WriteArray(arr3D);
+
+
+// Напишите программу, которая заполнит спирально массив 4 на 4.
+// Например, на выходе получается вот такой массив:
+// 01 02 03 04
+// 12 13 14 05
+// 11 16 15 06
+// 10 09 08 07
+
+
+int n = 4;
+int[,] spiralMatrix = new int[n, n];
+
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= spiralMatrix.GetLength(0) * spiralMatrix.GetLength(1))
 {
-    int[] temp = new int[arr3D.GetLength(0) * arr3D.GetLength(1) * arr3D.GetLength(2)];
+    spiralMatrix[i, j] = temp;
+    temp++;
     
-    int  number;
-    
-    for (int i = 0; i < temp.GetLength(0); i++)
+    if (i <= j + 1 && i + j < spiralMatrix.GetLength(1) - 1)
     {
-        temp[i] = new Random().Next(10, 100);
-        
-        number = temp[i];
-        
-        if (i >= 1)
-        {
-            for (int j = 0; j < i; j++)
-            {
-                while (temp[i] == temp[j])
-                {
-                    temp[i] = new Random().Next(10, 100);
-                    j = 0;
-                    number = temp[i];
-                }
-                number = temp[i];
-            }
-        }
+        j++;
     }
     
-    int count = 0; 
-    
-    for (int x = 0; x < arr3D.GetLength(0); x++)
+    else if (i < j && i + j >= spiralMatrix.GetLength(0) - 1)
     {
-        for (int y = 0; y < arr3D.GetLength(1); y++)
-        {
-            for (int z = 0; z < arr3D.GetLength(2); z++)
-            {
-                arr3D[x, y, z] = temp[count];
-                count++;
-            }
-        }   
+        i++;
+    }
+    
+    else if (i >= j && i + j > spiralMatrix.GetLength(1) - 1)
+    {
+        j--;
+    }
+    
+    else
+    {
+        i--;
     }
 }
 
-void WriteArray (int[,,] arr3D)
+void WriteArray (int[,] array)
 {
-    for (int i = 0; i < arr3D.GetLength(0); i++)
+    for (int i = 0; i < array.GetLength(0); i++)
     {
-        for (int j = 0; j < arr3D.GetLength(1); j++)
+        for (int j = 0; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < arr3D.GetLength(2); k++)
+            if (array[i,j] / 10 <= 0)
             {
-                Console.Write( $"{arr3D[i,j,k]}({k},{i},{j})  ");
+                Console.Write($" {array[i,j]} ");
             }
-            Console.WriteLine();
-        }
-        Console.WriteLine();
+            else 
+            {
+                Console.Write($"{array[i,j]} ");
+            }
     }
+    Console.WriteLine();
+  }
 }
 
-FillArray(arr3D);
-WriteArray(arr3D);
+WriteArray(spiralMatrix);
