13494713983ec645cf0697d4eb077229210289e9
Boris Drybin <makoh93@gmail.com>
2023-05-24 19:56:49 +0300

    third task added

--- Program.cs
+++ Program.cs
@@ -80,64 +80,146 @@
 // выдаёт номер строки с наименьшей суммой элементов: 1 строка
 
 
-Console.WriteLine("Введите количество строк массива");
-int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество строк массива");
+// int rows = Convert.ToInt32(Console.ReadLine());
 
-Console.WriteLine("Введите количество столбцов массива");
-int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите количество столбцов массива");
+// int columns = Convert.ToInt32(Console.ReadLine());
 
-int[,] arr = new int [rows, columns];
+// int[,] arr = new int [rows, columns];
 
-void GetArray()
+// void GetArray()
+// {
+//     for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             arr[i, j] = new Random().Next(1, 10);
+//         }
+//     }
+// }
+
+// void PrintArray()
+// {
+//    for(int i = 0; i < arr.GetLength(0); i++)
+//     {
+//         for(int j = 0; j < arr.GetLength(1); j++)
+//         {
+//             Console.Write(arr[i, j] + " ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void SumRow(int[,] array)
+// {
+//     int minSum = 0;
+//     int index = 0;
+
+//     for(int i = 0; i < arr.GetLength(0); i++)
+//         {
+//             int sum = 0;
+
+//             for(int j = 0; j < arr.GetLength(1); j++)
+//                 {
+//                     sum = sum + arr[i, j];
+//                 }
+//             if(i == 0)
+//                 {
+//                     minSum = sum;
+//                 }
+//             else if(sum < minSum)
+//                 {
+//                     minSum = sum;
+//                     index = i;
+//                 }
+//         }
+//         Console.WriteLine("Номер строки с минимальной суммой: " +(index + 1));
+// }
+
+// GetArray();
+// PrintArray();
+// SumRow(arr);
+
+
+// Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.WriteLine("Введите количество строк первой матрицы");
+int rows1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов первой матрицы");
+int columns1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество строк второй матрицы");
+int rows2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите количество столбцов второй матрицы");
+int columns2 = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix1 = new int [rows1, columns1];
+int[,] matrix2 = new int [rows2, columns2];
+int[,] resultMatrix = new int [rows1, columns2];
+
+void GetMatrix(int[,] matrix)
 {
-    for(int i = 0; i < arr.GetLength(0); i++)
+    for(int i = 0; i < matrix.GetLength(0); i++)
     {
-        for(int j = 0; j < arr.GetLength(1); j++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
         {
-            arr[i, j] = new Random().Next(1, 10);
+            matrix[i, j] = new Random().Next(1, 10);
         }
     }
 }
 
-void PrintArray()
+void PrintMatrix(int[,] matrix)
 {
-   for(int i = 0; i < arr.GetLength(0); i++)
+   for(int i = 0; i < matrix.GetLength(0); i++)
     {
-        for(int j = 0; j < arr.GetLength(1); j++)
+        for(int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write(arr[i, j] + " ");
+            Console.Write(matrix[i, j] + " ");
         }
         Console.WriteLine();
     }
     Console.WriteLine();
 }
 
-void SumRow(int[,] array)
+void ProductMatrix(int[,] matrix1, int[,] matrix2, int[,] resultMatrix)
 {
-    int minSum = 0;
-    int index = 0;
-
-    for(int i = 0; i < arr.GetLength(0); i++)
+    if(rows2 != columns1)
+    {
+        Console.WriteLine("Перемножение матриц невозможно!");
+    }
+    
+    else
+    {
+        for(int i = 0; i < resultMatrix.GetLength(0); i++)
         {
-            int sum = 0;
+            for(int j = 0; j < resultMatrix.GetLength(1); j++)
+            {
+                int sum = 0;
 
-            for(int j = 0; j < arr.GetLength(1); j++)
+                for(int k = 0; k < matrix1.GetLength(1); k++)
                 {
-                    sum = sum + arr[i, j];
+                    sum += matrix1[i, k] * matrix2[k, j];
                 }
-            if(i == 0)
-                {
-                    minSum = sum;
-                }
-            else if(sum < minSum)
-                {
-                    minSum = sum;
-                    index = i;
-                }
+                    resultMatrix[i, j] = sum;
+            }
         }
-        Console.WriteLine("Номер строки с минимальной суммой: " +(index + 1));
+    }
 }
 
-GetArray();
-PrintArray();
-SumRow(arr);
+GetMatrix(matrix1);
+PrintMatrix(matrix1);
+GetMatrix(matrix2);
+PrintMatrix(matrix2);
+ProductMatrix(matrix1, matrix2, resultMatrix);
+PrintMatrix(resultMatrix);
